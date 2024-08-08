eea014f220d66e7ee96ab3aa9b752e5ef486661b
tokarski-to <Tomasz.Tokarski@esk-sic.de>
2024-08-08 11:26:19 +0200

    Add credit card data and security check, add spa option

--- exercise/main-ex.py
+++ exercise/main-ex.py
@@ -10,27 +10,28 @@ class Product:
         self.product_id = product_id
         self.name = df.loc[df['id'] == self.product_id, 'name'].squeeze().title()
         self.price = df.loc[df['id'] == self.product_id, 'price'].squeeze()
-        self.stock = df.loc[df['id'] == self.product_id, 'in stock'].squeeze()
 
-    def buy(self):
-        stock = self.stock
-        if stock > 0:
-            stock = stock - 1
-            df.loc[df['id'] == self.product_id, 'in stock'] = stock
-            df.to_csv('articles.csv', index=False)
+    def available(self):
+        in_stock = df.loc[df['id'] == self.product_id, 'in stock'].squeeze()
+        return in_stock
 
-    def invoice(self):
+
+class Invoice:
+    def __init__(self, article):
+        self.article = article
+
+    def generate(self):
         pdf = FPDF(orientation="P", unit="mm", format="A4")
         pdf.add_page()
 
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"Receipt nr.{self.product_id}", ln=1)
+        pdf.cell(w=50, h=8, txt=f"Receipt nr.{self.article.product_id}", ln=1)
 
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"Article: {self.name}", ln=1)
+        pdf.cell(w=50, h=8, txt=f"Article: {self.article.name}", ln=1)
 
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"Price: {self.price}", ln=1)
+        pdf.cell(w=50, h=8, txt=f"Price: {self.article.price}", ln=1)
 
         pdf.output("receipt.pdf")
 
@@ -38,7 +39,9 @@ class Product:
 print(df)
 
 product_ID = input('Choose an ID of the product: ')
-
 product = Product(product_ID)
-product.invoice()
-product.buy()
+if product.available():
+    invoice = Invoice(product)
+    invoice.generate()
+else:
+    print('Product not available')
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('hotels.csv', sep=',', dtype={'id': str})
-
+df_cards = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
+df_cards_security = pd.read_csv('card_security.csv', dtype=str)
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -21,6 +22,26 @@ class Hotel:
         df.to_csv('hotels.csv', index=False)
 
 
+class SpaHotel(Hotel):
+    def book_spa_package(self):
+        pass
+
+
+class SpaTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for your SPA reservation!
+        Here are your SPA booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
+
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
         self.customer_name = customer_name
@@ -36,14 +57,50 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {'number': self.number, 'expiration': expiration,
+                     'holder': holder, 'cvc': cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+class SecureCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security['number'] == self.number, 'password'].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
+
 print(df)
 
 hotel_ID = input('Enter the ID of the hotel: ')
-hotel = Hotel(hotel_ID)
+hotel = SpaHotel(hotel_ID)
 if hotel.available():
-    hotel.book()
-    name = input('Enter your name: ')
-    reservation_ticket = ReservationTicket(name, hotel)
-    print(reservation_ticket.generate())
+    credit_card = SecureCreditCard(number='1234567890123456')
+    if credit_card.validate(expiration='12/26', holder='JOHN SMITH', cvc='123'):
+        if credit_card.authenticate(given_password='mypass'):
+            hotel.book()
+            name = input('Enter your name: ')
+            reservation_ticket = ReservationTicket(name, hotel)
+            print(reservation_ticket.generate())
+
+            spa_answer = input('Do you want to reserve a SPA package?: ')
+            if spa_answer == 'yes':
+                hotel.book_spa_package()
+                spa_ticket = SpaTicket(name, hotel)
+                print(spa_ticket.generate())
+
+        else:
+            print('Credit card authentication failed.')
+    else:
+        print('There was a problem with your payment.')
 else:
     print('Hotel is not free.')
